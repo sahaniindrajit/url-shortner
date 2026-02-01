b1f89856b324cf051aff8f90c78cc9f30838b137
Indrajit sahani <indrajit38mig@gmail.com>
2026-02-01 21:59:30 +0530

    added server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"url-shortner/internal/config"
+	"url-shortner/internal/handler"
+	"url-shortner/internal/service"
+	"url-shortner/internal/store"
 
 	"github.com/joho/godotenv"
 )
@@ -14,5 +18,21 @@ func main() {
 	}
 
 	cfg := config.Load()
+
+	port := cfg.Port
+	baseURL := cfg.BaseURL + port
+
+	MemoryStore := store.NewMemoryStore()
+	shortenerService := service.NewShortener(MemoryStore)
+
+	shortenHandler := handler.NewShortenHandler(shortenerService, baseURL)
+	redirectHandler := handler.NewRedirectHandler(MemoryStore)
+
+	mux := http.NewServeMux()
+	mux.Handle("/shorten", shortenHandler)
+	mux.Handle("/", redirectHandler)
+
+	log.Printf("🚀 URL shortener running on %s\n", baseURL)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 
 }
